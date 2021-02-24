dd3ab9853629b8d46341a1ec4bcfeac8c6af1a58
Matt Newbigging <mnewbigging@opus2.com>
2021-02-24 20:08:32 +0000

    fixed strictmode error, added font import

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './App';
 import { WordBash } from './WordBash';
 
 import './app.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <WordBash />
-  </React.StrictMode>,
-  document.getElementById('app-root')
-);
+ReactDOM.render(<WordBash />, document.getElementById('app-root'));
