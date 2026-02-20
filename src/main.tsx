bb99a3faebe3390d77e2e39d0ed688e7033147da
Matt Newbigging <mrnewbigging@gmail.com>
2026-02-20 15:05:02 +0000

    removed root div

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import ReactDOM from "react-dom/client";
 
 import { App } from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.body as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
