c88977d69b74b02ba54929ad9c4ac65f83897935
Matt Newbigging <mrnewbigging@gmail.com>
2026-02-20 18:21:27 +0000

    game screen comp

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { StartScreen } from "./start-screen/start-screen";
 
 export function App() {
+  const [started, setStarted] = useState(false);
+
+  function onStart() {
+    setStarted(true);
+  }
+
   return (
     <>
       <StartScreen />
---
+++ src/components/game-screen/game-screen.tsx
@@ -0,0 +1,5 @@
+import "./game-screen.scss";
+
+export function GameScreen() {
+  return <div className="game-screen"></div>;
+}
