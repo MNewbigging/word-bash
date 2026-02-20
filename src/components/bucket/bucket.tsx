853df7696d721fda21cf17b1b0d25b723398fd94
Matt Newbigging <mrnewbigging@gmail.com>
2026-02-20 19:40:28 +0000

    danger zone styling

--- src/components/bucket/bucket.tsx
+++ src/components/bucket/bucket.tsx
@@ -1,5 +1,11 @@
 import "./bucket.scss";
 
 export function Bucket() {
-  return <div className="bucket"></div>;
+  return (
+    <div className="bucket ">
+      <div className="danger-overlay">
+        <div className="lethal-row"></div>
+      </div>
+    </div>
+  );
 }
