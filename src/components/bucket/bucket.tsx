ebe5bfc6584d4edae62d65fe9db8ac1782a8ff1f
Matt Newbigging <mrnewbigging@gmail.com>
2026-02-21 15:15:49 +0000

    tidying

--- src/components/bucket/bucket.tsx
+++ src/components/bucket/bucket.tsx
@@ -1,5 +1,5 @@
-import { COLS, Game, Letter, LetterTile, ROWS } from "../../game";
-import { useEventUpdater } from "../hooks/use-event-updater";
+import { Game } from "../../game";
+import { TileLayer } from "../tile-layer/tile-layer";
 import "./bucket.scss";
 
 interface BucketProps {
@@ -14,54 +14,6 @@ export function Bucket({ game }: BucketProps) {
       </div>
 
       <TileLayer game={game} />
-    </div>
-  );
-}
-
-type RenderTile = {
-  letterTile: LetterTile;
-  col: number;
-  row: number;
-};
-
-interface TileLayerProps {
-  game: Game;
-}
-
-function TileLayer({ game }: TileLayerProps) {
-  useEventUpdater("grid-changed");
-
-  // Flatten grid to get list of tiles to render
-  const grid = game.grid;
-  const tiles: RenderTile[] = [];
-  for (let col = 0; col < COLS; col++) {
-    const column = grid[col];
-
-    // Go over all existing rows in this column
-    for (let i = 0; i < column.length; i++) {
-      const letterTile = column[i];
-      const row = ROWS - 1 - i; // Flip from top-down to bottom-up
-      tiles.push({ letterTile, col, row });
-    }
-  }
-
-  return (
-    <div className="tile-layer">
-      {tiles.map(({ letterTile, col, row }) => {
-        return (
-          <div
-            key={letterTile.id}
-            className="tile"
-            style={{
-              ["--tx" as any]: `${col * 100}%`,
-              ["--ty" as any]: `${row * 100}%`,
-              ["--tile-bg" as any]: letterTile.color,
-            }}
-          >
-            {letterTile.letter}
-          </div>
-        );
-      })}
     </div>
   );
 }
---
+++ src/components/tile-layer/tile-layer.tsx
@@ -0,0 +1,51 @@
+import { COLS, Game, LetterTile, ROWS } from "../../game";
+import { useEventUpdater } from "../hooks/use-event-updater";
+import "./tile-layer.scss";
+
+type RenderTile = {
+  letterTile: LetterTile;
+  col: number;
+  row: number;
+};
+
+interface TileLayerProps {
+  game: Game;
+}
+
+export function TileLayer({ game }: TileLayerProps) {
+  useEventUpdater("grid-changed");
+
+  // Flatten grid to get list of tiles to render
+  const grid = game.grid;
+  const tiles: RenderTile[] = [];
+  for (let col = 0; col < COLS; col++) {
+    const column = grid[col];
+
+    // Go over all existing rows in this column
+    for (let i = 0; i < column.length; i++) {
+      const letterTile = column[i];
+      const row = ROWS - 1 - i; // Flip from top-down to bottom-up
+      tiles.push({ letterTile, col, row });
+    }
+  }
+
+  return (
+    <div className="tile-layer">
+      {tiles.map(({ letterTile, col, row }) => {
+        return (
+          <div
+            key={letterTile.id}
+            className="tile"
+            style={{
+              ["--tx" as any]: `${col * 100}%`,
+              ["--ty" as any]: `${row * 100}%`,
+              ["--tile-bg" as any]: letterTile.color,
+            }}
+          >
+            {letterTile.letter}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
